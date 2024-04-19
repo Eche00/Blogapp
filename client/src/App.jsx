a9290493f0fc3c02ed7332828ae2f899531cfa44
Eche00 <echeeze956@gmail.com>
2024-04-19 13:03:31 +0100

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1 className=" font-bold text-5xl text-center">MY BLOG APP</h1>
+    </div>
+  );
+}
+
+export default App;
