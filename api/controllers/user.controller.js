8744cccc1c9c5e31879a5855d1e421169d38986c
Eche00 <echeeze956@gmail.com>
2024-05-12 12:39:54 +0100

    full update user api completed minus bio

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,69 @@
+import User from "../modules/user.module.js";
+import { errorHandler } from "../utils/error.js";
+import bcryptjs from "bcryptjs";
+
 export const testControls = (req, res) => {
   res.json({ message: "api active" });
+};
+
+export const updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.userId) {
+    return next(errorHandler(403, "You are not allowed to update this user"));
+  }
+  if (req.body.password) {
+    if (req.body.password.length < 6) {
+      return next(errorHandler(400, "Password must be at least 6 characters"));
+    }
+    req.body.password = bcryptjs.hashSync(req.body.password, 10);
+  }
+  if (req.body.username) {
+    if (req.body.username.length < 6 || req.body.username.length > 15) {
+      return next(
+        errorHandler(
+          400,
+          "Username must be at least min of 6 & max of 15 characters"
+        )
+      );
+    }
+    if (req.body.username.includes(" ")) {
+      return next(errorHandler(400, "Username cannot contain spaces"));
+    }
+    if (req.body.username !== req.body.username.toLowerCase()) {
+      return next(errorHandler(400, "username must be lowercase"));
+    }
+    if (!req.body.username.match(/[a-zA-Z0-9]+$/)) {
+      return next(
+        errorHandler(400, "username contains only letters and numbers")
+      );
+    }
+    if (req.body.bio) {
+      if (req.body.bio.length < 20 || req.body.bio.length > 100) {
+        return next(
+          errorHandler(
+            400,
+            "Username must be at least min of 20 & max of 100 characters"
+          )
+        );
+      }
+    }
+    try {
+      const updateUser = await User.findByIdAndUpdate(
+        req.params.userId,
+        {
+          $set: {
+            username: req.body.username,
+            bio: req.body.bio,
+            email: req.body.email,
+            password: req.body.password,
+            avatar: req.body.avatar,
+          },
+        },
+        { new: true }
+      );
+      const { password, ...rest } = updateUser._doc;
+      res.status(200).json(rest);
+    } catch (error) {
+      next(error);
+    }
+  }
 };
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,7 +1,10 @@
 import express from "express";
-import { testControls } from "../controllers/user.controller.js";
+import { testControls, updateUser } from "../controllers/user.controller.js";
+import { verifyToken } from "../utils/verifiedUser.js";
 
 const router = express.Router();
 
 router.get("/test", testControls);
+router.put("/update/:userId", verifyToken, updateUser);
+
 export default router;
