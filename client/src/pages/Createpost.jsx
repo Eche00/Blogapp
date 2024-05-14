47a6e61f0a90f3410de0f4495b181dedb589fbd7
Eche00 <echeeze956@gmail.com>
2024-05-14 16:53:46 +0100

    complete create profile ui

--- client/src/pages/Createpost.jsx
+++ client/src/pages/Createpost.jsx
@@ -1,7 +1,58 @@
 import React from "react";
+import { Button, FileInput, Select, TextInput } from "flowbite-react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 function Createpost() {
-  return <div>Createpost</div>;
+  return (
+    <div>
+      <div className=" max-w-3xl mx-auto p-3 ">
+        <h1 className=" text-center  text-3xl font-serif sm:pt-10 pt-5 font-bold">
+          Create Post
+        </h1>
+        <form className=" flex flex-col gap-4 my-20">
+          <div className=" flex flex-col sm:flex-row gap-4 justify-between">
+            <TextInput
+              type="text"
+              placeholder="Title"
+              id="title"
+              className="flex-1"
+              required
+            />
+            <Select>
+              <option value="uncategorized">Select a category</option>
+              <option value="javascript">Javascript</option>
+              <option value="typescript">Typescript</option>
+              <option value="react">React</option>
+              <option value="nextjs">Next.Js</option>
+              <option value="nodejs">Node.Js</option>
+              <option value="php">PHP</option>
+              <option value="css">CSS</option>
+              <option value="tailwind">Tailwind</option>
+            </Select>
+          </div>
+          <div className="flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3">
+            <FileInput type="file" accept="image/*" />
+            <Button type="button" gradientDuoTone="purpleToBlue" size="sm">
+              Upload image
+            </Button>
+          </div>
+          <ReactQuill
+            theme="snow"
+            placeholder="Write your article..."
+            className=" h-80 mb-12"
+            required
+          />
+          <Button
+            type="submit"
+            gradientDuoTone="purpleToPink"
+            className=" my-12 sm:my-0">
+            Publish
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Createpost;
