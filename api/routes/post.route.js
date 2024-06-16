96b200f28a3fd31e805af38ffd6fd6f118c76441
Eche00 <echeeze956@gmail.com>
2024-06-16 23:37:22 +0100

    create get post api route

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,9 +1,10 @@
-import { createPost } from "../controllers/post.controller.js";
+import { createPost, getPosts } from "../controllers/post.controller.js";
 import express from "express";
 import { verifyToken } from "../utils/verifiedUser.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, createPost);
+router.get("/getposts", getPosts);
 
 export default router;
